721f091e29d76d5cd9d0cdaad3644a755ae6ff58
spkal <spk@seapowet.org>
2017-03-24 11:37:53 -0700

    Also look at flights from Seattle.

--- read.R
+++ read.R
@@ -1,8 +1,18 @@
-# library("readr")
-# sea <- read_csv("data/SEA.csv")
-# indxDest <- which(sea["Dest"] == "SEA")
-# sea <- sea[indxDest,]
-sea <- readRDS("FlightsToSeattle.rds")
+sea <- readr::read_csv("data/SEA.csv")
+indxDest <- which(sea[["Dest"]] == "SEA")
+FlightsToSeattle <- sea[indxDest, ]
+indxOrig <- which(sea[["Origin"]] == "SEA")
+FlightsFromSeattle <- sea[indxOrig, ]
+saveRDS(FlightsToSeattle, "FlightsToSeattle.rds")
+saveRDS(FlightsFromSeattle, "FlightsFromSeattle.rds")
+FlightsToSeattle <- readRDS("FlightsToSeattle.rds")
+FlightsFromSeattle <- readRDS("FlightsFromSeattle.rds")
 # Drop obs with missing ArrTime:
-sea <- sea[!is.na(sea[["ArrTime"]]),]
-fracHour <- sea[["ArrTime"]] %/% 100 + (sea[["ArrTime"]] %% 100) / 60
+FlightsToSeattle <-
+  FlightsToSeattle[!is.na(FlightsToSeattle[["ArrTime"]]),]
+FlightsFromSeattle <-
+  FlightsFromSeattle[!is.na(FlightsFromSeattle[["DepTime"]]),]
+fracArrHour <- FlightsToSeattle[["ArrTime"]] %/% 100
+  + (FlightsToSeattle[["ArrTime"]] %% 100) / 60
+fracDepHour <- FlightsToSeattle[["DepTime"]] %/% 100
+  + (FlightsToSeattle[["DepTime"]] %% 100) / 60
